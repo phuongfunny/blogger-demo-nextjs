c2607966b28eadece73a4f0649d195aecb853fdf
phuongtechchain <phuong.vv@techchain.vn>
2021-09-29 10:46:00 +0700

    fix: _document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
   render() {
     return (
       <Html>
